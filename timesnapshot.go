628793f1d0936e1137470fde03ec44ad2b0bac31
Pranjal Soni <soni.pranjals17@gmail.com>
2022-04-01 17:12:43 +0530

    Move TimeSnapshot into timesnapshot.go

---
+++ timesnapshot.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type TimeSnapshot struct {
+	TotalSeconds int
+}
+
+// Return error if sec/min field are not less than 60
+func CheckField(sec, min int, checkMin bool) error {
+	var err error
+	if checkMin && min >= 60 && sec >= 60 {
+		err = fmt.Errorf("minute's and second's")
+	} else if checkMin && min >= 60 {
+		err = fmt.Errorf("minute's")
+	} else if sec >= 60 {
+		err = fmt.Errorf("second's")
+	}
+	if err != nil {
+		err = fmt.Errorf("%v field must be less than 60", err)
+	}
+	return err
+}
+
+// Returns a new TimeSnapshot whose TotalSeconds is equal to the
+// total seconds in some snapshot of format [[hh:]mm:]ss
+func New(snapshot string) (*TimeSnapshot, error) {
+	var hr, min, sec int
+
+	if m, err := regexp.MatchString(`^\d*$`, snapshot); m {
+		if err != nil {
+			return &TimeSnapshot{}, err
+		}
+		sec, _ = strconv.Atoi(snapshot)
+	} else if m, err := regexp.MatchString(`^\d+:\d{2}$`, snapshot); m {
+		if err != nil {
+			return &TimeSnapshot{}, err
+		}
+		s := strings.Split(snapshot, ":")
+		min, _ = strconv.Atoi(s[0])
+		sec, _ = strconv.Atoi(s[1])
+		if err = CheckField(sec, min, false); err != nil {
+			return &TimeSnapshot{}, err
+		}
+	} else if m, err := regexp.MatchString(`^\d+:\d{2}:\d{2}$`, snapshot); m {
+		if err != nil {
+			return &TimeSnapshot{}, err
+		}
+		s := strings.Split(snapshot, ":")
+		hr, _ = strconv.Atoi(s[0])
+		min, _ = strconv.Atoi(s[1])
+		sec, _ = strconv.Atoi(s[2])
+		if err = CheckField(sec, min, true); err != nil {
+			return &TimeSnapshot{}, err
+		}
+	} else {
+        return &TimeSnapshot{}, fmt.Errorf("duration must be in [[hh:]mm:]ss format")
+	}
+	return &TimeSnapshot{TotalSeconds: (hr * 3600) + (min * 60) + sec}, nil
+}
+
+func (t TimeSnapshot) String() string {
+	hrs := t.TotalSeconds / 3600
+	min := (t.TotalSeconds / 60) % 60
+	sec := t.TotalSeconds % 60
+	return fmt.Sprintf("%02d:%02d:%02d", hrs, min, sec)
+}
+
+// Countup from some time instant t till infinity
+func (t TimeSnapshot) Countup() {
+	for rsec := t.TotalSeconds; ; rsec++ {
+		fmt.Printf("%s\r", TimeSnapshot{TotalSeconds: rsec})
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// Countdown from some time instant t till zero seconds
+func (t TimeSnapshot) Countdown() {
+	for rsec := t.TotalSeconds; rsec > 0; rsec-- {
+		fmt.Printf("%s\r", TimeSnapshot{TotalSeconds: rsec})
+		time.Sleep(1 * time.Second)
+	}
+}
+
