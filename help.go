f589234e6dafaa7ec56b488e01e0e88d7ab86ea1
Pranjal Soni <soni.pranjals17@gmail.com>
2022-05-20 20:26:36 +0530

    Feat: `NewHelpView()`

--- help.go
+++ help.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
@@ -8,4 +9,24 @@ type HelpView struct {
 	*tview.TextView
 	globals, locals []*Binding
 	title           string
+}
+
+func NewHelpView() *HelpView {
+	hv := &HelpView{
+		TextView: tview.NewTextView(),
+		title:    " Help ",
+	}
+	hv.
+		SetChangedFunc(func() {
+			wtc.app.Draw()
+		}).
+		SetTextAlign(tview.AlignCenter).
+		SetTitleAlign(tview.AlignLeft).
+		SetBorder(true).
+		SetBackgroundColor(tcell.ColorDefault).
+		SetFocusFunc(focusFunc(hv)).
+		SetBlurFunc(blurFunc(hv)).
+		SetTitle(hv.title)
+
+	return hv
 }
