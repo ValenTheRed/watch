f515d77c789548788f5f02b73faca5bc6fa01dea
Pranjal Soni <soni.pranjals17@gmail.com>
2022-05-21 22:30:55 +0530

    Change: split up `utils.go`

---
+++ focuser.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+)
+
+type Focuser interface {
+	Title() string
+	SetTitle(string) *tview.Box
+
+	SetBorderColor(tcell.Color) *tview.Box
+	SetTitleColor(tcell.Color) *tview.Box
+}
+
+func focusFunc(widget Focuser, km KeyMaper) func() {
+	return func() {
+		widget.
+			SetTitle("[" + widget.Title() + "]").
+			SetTitleColor(tcell.ColorOrange).
+			SetBorderColor(tcell.ColorOrange)
+		wtc.help.SetLocals(km)
+
+		// FIXME: UpdateDisplay is bugged. Or rather, my approach to the
+		// way all the widgets update the their text is flawed.
+		//
+		// If `wtc.help` executes this function then, the main
+		// application goroutine gets blocked and the program halts. Why
+		// does this happen? Speculating: probably because of the way
+		// TextView widgets -- which `wtc.help` is -- run the handler
+		// installed using `SetChangedFunc`.
+		// [Concurrency in tview](https://github.com/rivo/tview/wiki/Concurrency)
+		wtc.help.UpdateDisplay()
+	}
+}
+
+func blurFunc(widget Focuser) func() {
+	return func() {
+		widget.
+			SetTitle(widget.Title()).
+			SetTitleColor(tview.Styles.TitleColor).
+			SetBorderColor(tview.Styles.BorderColor)
+		wtc.help.UnsetLocals()
+
+		// FIXME: UpdateDisplay is bugged. Same as `focusFunc()`
+		wtc.help.UpdateDisplay()
+	}
+}
--- utils/utils.go
+++ utils/utils.go
@@ -2,70 +2,8 @@ package utils
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
-	"strings"
 	"time"
-
-	"github.com/gdamore/tcell/v2"
-	"github.com/rivo/tview"
 )
-
-// checkField returns error if sec/min field are not less than 60.
-func checkField(sec, min int) error {
-	var errmsg string
-	if sec >= 60 {
-		errmsg = "second's"
-	}
-	if min >= 60 && errmsg == "" {
-		errmsg = "minute's"
-	} else if min >= 60 {
-		errmsg += " and minute's"
-	}
-	if errmsg != "" {
-		return fmt.Errorf("%v field must be less than 60", errmsg)
-	}
-	return nil
-}
-
-// ParseDuration returns the total number of seconds in dur, which must
-// be of format [[hh:]mm:]ss.
-func ParseDuration(dur string) (int, error) {
-	var hr, min, sec int
-
-	if m, err := regexp.MatchString(`^\d*$`, dur); m {
-		if err != nil {
-			return 0, err
-		}
-		sec, _ = strconv.Atoi(dur)
-	} else if m, err := regexp.MatchString(`^\d+:\d{2}$`, dur); m {
-		if err != nil {
-			return 0, err
-		}
-		s := strings.Split(dur, ":")
-		min, _ = strconv.Atoi(s[0])
-		sec, _ = strconv.Atoi(s[1])
-		// it's okay for minute field to be more than 60
-		if err = checkField(sec, 0); err != nil {
-			return 0, err
-		}
-	} else if m, err := regexp.MatchString(`^\d+:\d{2}:\d{2}$`, dur); m {
-		if err != nil {
-			return 0, err
-		}
-		s := strings.Split(dur, ":")
-		hr, _ = strconv.Atoi(s[0])
-		min, _ = strconv.Atoi(s[1])
-		sec, _ = strconv.Atoi(s[2])
-		if err = checkField(sec, min); err != nil {
-			return 0, err
-		}
-	} else {
-		return 0, fmt.Errorf("duration must be in [[hh:]mm:]ss format")
-	}
-
-	return (hr * 3600) + (min * 60) + sec, nil
-}
 
 func FormatSecond(s int) string {
 	hrs := s / 3600
@@ -85,47 +23,5 @@ func worker(work func(), quit <-chan struct{}) {
 		case <-quit:
 			return
 		}
-	}
-}
-
-type Focuser interface {
-	Title() string
-	SetTitle(string) *tview.Box
-
-	SetBorderColor(tcell.Color) *tview.Box
-	SetTitleColor(tcell.Color) *tview.Box
-}
-
-func focusFunc(widget Focuser, km KeyMaper) func() {
-	return func() {
-		widget.
-			SetTitle("[" + widget.Title() + "]").
-			SetTitleColor(tcell.ColorOrange).
-			SetBorderColor(tcell.ColorOrange)
-		wtc.help.SetLocals(km)
-
-		// FIXME: UpdateDisplay is bugged. Or rather, my approach to the
-		// way all the widgets update the their text is flawed.
-		//
-		// If `wtc.help` executes this function then, the main
-		// application goroutine gets blocked and the program halts. Why
-		// does this happen? Speculating: probably because of the way
-		// TextView widgets -- which `wtc.help` is -- run the handler
-		// installed using `SetChangedFunc`.
-		// [Concurrency in tview](https://github.com/rivo/tview/wiki/Concurrency)
-		wtc.help.UpdateDisplay()
-	}
-}
-
-func blurFunc(widget Focuser) func() {
-	return func() {
-		widget.
-			SetTitle(widget.Title()).
-			SetTitleColor(tview.Styles.TitleColor).
-			SetBorderColor(tview.Styles.BorderColor)
-		wtc.help.UnsetLocals()
-
-		// FIXME: UpdateDisplay is bugged. Same as `focusFunc()`
-		wtc.help.UpdateDisplay()
 	}
 }
