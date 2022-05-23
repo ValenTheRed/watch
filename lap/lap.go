c643f3687d55ea9086fa18709189f8b8bd007a09
Pranjal Soni <soni.pranjals17@gmail.com>
2022-05-23 15:31:27 +0530

    Feat: constructor for `Lap`

--- lap/lap.go
+++ lap/lap.go
@@ -1,6 +1,7 @@
 package lap
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 
 	"github.com/ValenTheRed/watch/help"
@@ -18,4 +19,40 @@ type Lap struct {
 
 	sw  *stopwatch.Stopwatch
 	app *tview.Application
+}
+
+func New(sw *stopwatch.Stopwatch, app *tview.Application) *Lap {
+	l := &Lap{
+		title: " Lap ",
+		Table: tview.NewTable(),
+
+		app: app,
+		sw:  sw,
+		km: keyMap{
+			Lap: help.NewBinding(
+				help.WithRune('l'), help.WithHelp("Lap"),
+			),
+			Copy: help.NewBinding(
+				help.WithRune('c'), help.WithHelp("Copy all"),
+			),
+		},
+	}
+
+	l.
+		SetFixed(1, 0).
+		SetSelectable(true, false).
+		SetTitleAlign(tview.AlignLeft).
+		SetBackgroundColor(tcell.ColorDefault).
+		SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+			switch event.Rune() {
+			case l.km.Lap.Rune():
+				// TODO: Add new row with lap info
+			case l.km.Copy.Rune():
+				// TODO: Copy to system clipboard
+			}
+			return event
+		}).
+		SetTitle(l.title)
+
+	return l
 }
