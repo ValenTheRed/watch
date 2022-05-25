746bea95320dcaa197a962de3e3dbc9850109624
Pranjal Soni <soni.pranjals17@gmail.com>
2022-05-25 20:41:39 +0530

    Change: move `newLapCell()` into a utils.go
    Since it is being used by both, stopwatch.go and laps.go

--- stopwatch/laps.go
+++ stopwatch/laps.go
@@ -140,11 +140,3 @@ func (l *laps) reset() {
 	// So, we reselect the first row.
 	l.Select(0, 0)
 }
-
-// newLapCell returns a Table cell with a default style for a laps cell
-// applied.
-func newLapCell(text string, ref interface{}) *tview.TableCell {
-	return tview.NewTableCell(text).
-		SetReference(ref).
-		SetAlign(tview.AlignCenter)
-}
