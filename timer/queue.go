c7eb603524b2c97dd7db94782aee25778dff4e72
Pranjal Soni <soni.pranjals17@gmail.com>
2022-05-26 11:49:53 +0530

    Feat: add `newQueue()`

--- timer/queue.go
+++ timer/queue.go
@@ -1,12 +1,28 @@
 package timer
 
 import (
-	"github.com/ValenTheRed/watch/help"
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+
+	"github.com/ValenTheRed/watch/help"
 )
 
 type queue struct {
 	*tview.Table
 	km    map[string]*help.Binding
 	title string
+}
+
+// newQueue returns a new queue.
+func newQueue() *queue {
+	return &queue{
+		Table:  tview.NewTable(),
+		title: " Queue ",
+		km: map[string]*help.Binding{
+			"Select": help.NewBinding(
+				help.WithKey(tcell.KeyEnter),
+				help.WithHelp("Select"),
+			),
+		},
+	}
 }
