ca2e0b5139b24e3ee5908c53ce0d58f57ada7246
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-21 17:40:07 +0530

    Implement utility function getCenter()
    This will be useful in the Draw() functions of the various widgets that
    I'll be defining.

--- internal/utils.go
+++ internal/utils.go
@@ -6,3 +6,14 @@ const (
 	AlignUp
 	AlignDown
 )
+
+// getCenter returns the coordinate from where, if drawn, an object of
+// length reservedLen would looked centered on a screen of size
+// totalLen. Assuming 0 is the origin.
+// Eg.
+// total = 100, reserved = 10 => center = (100-10-1)/2 = floor(44.5) = 44
+// So, in the interval [0, 99], if drawn from point 44, an object of
+// length 10 will look centered.
+func getCenter(totalLen, reservedLen int) int {
+	return (totalLen - reservedLen - 1) / 2
+}
