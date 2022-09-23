7cb82f9f7bb9ff3d43e2e47ff329bc247acf523a
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-23 23:32:42 +0530

    Implement NewQueue()

--- internal/widget/queue.go
+++ internal/widget/queue.go
@@ -1,5 +1,12 @@
 package widget
 
+import (
+	"fmt"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
 type Queue struct {
 	*Table
 
@@ -9,4 +16,32 @@ type Queue struct {
 
 	// Format will be used to format seconds in durations column.
 	Format func(seconds int) string
+}
+
+const queueHeadIcon = "->"
+
+// NewQueue returns a new Queue.
+func NewQueue(durations ...int) *Queue {
+	q := &Queue{
+		Table:  NewTable("Queue", "Timer duration"),
+		head:   -1,
+		Format: SecondWithColons,
+	}
+
+	var newCell = func(text string, ref interface{}) *tview.TableCell {
+		c := tview.NewTableCell(text)
+		c.SetReference(ref)
+		c.SetAlign(tview.AlignCenter)
+		c.SetStyle(tcell.StyleDefault.Background(q.GetBackgroundColor()).Foreground(tcell.ColorWhite))
+		return c
+	}
+
+	for i, duration := range durations {
+		q.SetCell(i, 0, newCell(fmt.Sprint(i+1), i+1))
+		q.SetCell(i, 1, newCell(q.Format(duration), duration))
+	}
+	q.head = 0
+	q.GetCell(0, 0).SetText(queueHeadIcon)
+
+	return q
 }
