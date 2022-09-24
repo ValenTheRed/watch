bfb87b8dbef7f80055b96d5af9a015ccefa00963
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-24 14:56:48 +0530

    Implement NewLapTable()

--- internal/widget/laps.go
+++ internal/widget/laps.go
@@ -6,3 +6,13 @@ type LapTable struct {
 	// Format will be used to format the lap and total time.
 	Format func(seconds int) string
 }
+
+// NewLapTable returns a new LapTable. The seconds are formatted using
+// SecondWithColons by default.
+func NewLapTable() *LapTable {
+	t := NewTable("Lap", "Lap time", "Total")
+	return &LapTable{
+		Table: t,
+		Format: SecondWithColons,
+	}
+}
