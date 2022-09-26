5947712c4b25aefeb4c5aa9293f8ac5d32844cd0
Pranjal Soni <soni.pranjals17@gmail.com>
2022-09-26 23:19:44 +0530

    Implement MouseHandler()
    How would we select the buttons otherwise?

--- internal/widget/button_column.go
+++ internal/widget/button_column.go
@@ -1,6 +1,9 @@
 package widget
 
-import "github.com/rivo/tview"
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
 
 // ButtonColumn draws contained tview.Button in a columnar fashion,
 // like so (the borders may or may not be present):
@@ -20,4 +23,24 @@ func (bc *ButtonColumn) HasFocus() bool {
 		}
 	}
 	return bc.Box.HasFocus()
+}
+
+// MouseHandler returns the mouse handler for this primitive.
+func (bc *ButtonColumn) MouseHandler() func(tview.MouseAction, *tcell.EventMouse, func(p tview.Primitive)) (bool, tview.Primitive) {
+	return bc.WrapMouseHandler(func(action tview.MouseAction, event *tcell.EventMouse, setFocus func(p tview.Primitive)) (consumed bool, capture tview.Primitive) {
+		if !bc.InRect(event.Position()) {
+			return false, nil
+		}
+		// Pass mouse events along to the first button that takes it.
+		for _, b := range bc.buttons {
+			if b == nil {
+				continue
+			}
+			consumed, capture = b.MouseHandler()(action, event, setFocus)
+			if consumed {
+				return
+			}
+		}
+		return
+	})
 }
